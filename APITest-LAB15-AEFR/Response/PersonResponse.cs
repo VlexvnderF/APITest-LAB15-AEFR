90bc5b5eccdb246d71ea07ba9a1a18b83891f22a
VlexvnderF <alexander.flores.r@tecsup.edu.pe>
2023-07-09 12:39:06 -0500

    Agregar archivos de proyecto.

---
+++ APITest-LAB15-AEFR/Response/PersonResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace APITest_LAB15_AEFR.Response
+{
+    public class PersonResponse
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; } 
+    }
+}
